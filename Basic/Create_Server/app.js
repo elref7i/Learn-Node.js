e522886473422decb27a6e9d0b7b6690ac5ea9dc
Ahmed re <momo3956248@gmail.com>
2024-05-23 14:51:03 +0300

    EX 1

--- Basic/Create_Server/app.js
+++ Basic/Create_Server/app.js
@@ -3,24 +3,6 @@ const fs = require('fs');
 
 //EX 1
 
-const server = http.createServer((req,res)=>{
-    const URL = req.url;
-    res.setHeader('Conte-type','text/html');
-    res.write("<html>")
-    res.write('<head><title>Node</title></head>');
-    res.write('<body>');
-        res.write('<h1>Hello node js</h1>');
-        res.end();
-    res.write('</body>');
-    
-    res.write("</html>");
-    res.end();
-});
-server.listen(3000);
-
-
-//EX 2 
-
 /*
 
 const htmlpage =fs.readFileSync('./view/index.html','utf8');
@@ -41,6 +23,28 @@ const server = http.createServer((req,res)=> {
 server.listen(3000);
 
 */
+
+const htmlpage =fs.readFileSync('./view/index.html','utf8');
+const cssfile =fs.readFileSync('./view/style.css','utf8');
+const server = http.createServer((req,res)=> {
+    const URl = req.url;
+    if (URl === '/'){
+        res.write(htmlpage);
+    }
+    else if (URl === '/style.css' ){
+        res.write(cssfile)
+    }
+    else if (URl === '/message'){
+        res.write("<h1>default</h1>");
+    }
+    else{
+        res.write("not foound");
+    }
+    res.end();
+})
+server.listen(3000);
+
+
 
 
 
